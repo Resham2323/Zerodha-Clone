3a590858e10880794e5fa78e634af8b327caf9a4
Resham <reshamparveen0000@gmail.com>
2025-08-06 22:35:48 +0530

    Add Router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import HomePage from './LandingPage/Home/HomePage';
+import Signup from './LandingPage/Signup/Signup';
+import AboutPage from './LandingPage/About/AboutPage';
+import Pricing from './LandingPage/Pricing/Pricing';
+import  Product from './LandingPage/Product/Product';
+import  Support from './LandingPage/Support/Support'
 import './index.css';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-   <HomePage />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/Signup' element={<Signup/>}/>
+      <Route path='/About' element={<AboutPage/>}/>
+      <Route path='/Pricing' element={<Pricing/>}/>
+      <Route path='/Support' element={<Support/>}/>
+      <Route path='/Product' element={<Product/>}/>
+    </Routes>
+  </BrowserRouter>
 );
 
 
