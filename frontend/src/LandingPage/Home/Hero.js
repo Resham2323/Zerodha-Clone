ee5de6b448f41aa9231773ab9cd49bfa99152e3a
Resham <reshamparveen0000@gmail.com>
2025-09-04 17:46:59 +0530

    modify hero page

--- frontend/src/LandingPage/Home/Hero.js
+++ frontend/src/LandingPage/Home/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     return ( 
       <div className = "container p-5 text-center mb-5">
         <div className = "row">
-            <img src="media/homeHero.png" alt='HeroPage' className='mb-4 hero offset-1'/>
+            <img src='media/homeHero.png' alt='HeroImage' className='mb-4 hero offset-1'/>
             <h1 className='mt-3'>invest in everything</h1>
             <p>Online platform to invest in stocks, derivates, mutual funds, and more</p>
             <Link to={"/signup"}><button  className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin:"0 auto"}}>Signup now</button></Link> 
