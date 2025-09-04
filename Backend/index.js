1b7928bed9c5a6ae3bec39a25d3f0da816e513b7
Resham <reshamparveen0000@gmail.com>
2025-09-04 19:20:40 +0530

    add path

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 
