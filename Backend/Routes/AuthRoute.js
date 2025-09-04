c1d5ab43031e551b431f8067139a3d47c7e6f819
Resham <reshamparveen0000@gmail.com>
2025-09-04 19:52:08 +0530

    add middleware

--- Backend/Routes/AuthRoute.js
+++ Backend/Routes/AuthRoute.js
@@ -1,9 +1,15 @@
 const { Signup, Login } = require('../Controllers/AuthController');
-const {userVerification} =require('../Middleware/AuthMiddleware');
+const { userVerification } = require('../Middleware/AuthMiddleware');
 const router = require('express').Router();
 
-router.post('/',userVerification)
-router.post('/signup', Signup)
-router.post('/login', Login)
+// Public routes
+router.post('/signup', Signup);
+router.post('/login', Login);
 
-module.exports = router
+// Protected POST route
+router.post('/update-profile', userVerification, (req, res) => {
+  // You can access the user from req.user
+  res.json({ status: true, user: req.user.username, message: "Profile updated!" });
+});
+
+module.exports = router;
